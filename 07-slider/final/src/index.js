e3c0d7e3300b9f09a97a65b3681830ad645052c0
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-20 22:48:20 +0300

    slider project finished | React.js

--- 07-slider/final/src/index.js
+++ 07-slider/final/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Alternative from './Alternative';
-import Alternative2 from './Alternative2';
+// import Alternative from './Alternative';
+// import Alternative2 from './Alternative2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
